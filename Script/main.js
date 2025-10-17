3ec96687fc4655d53256bfe0f851ce04e8e24f81
Braian-jm <braianjmoura@gmail.com>
2025-10-16 22:55:26 -0300

    sobre nos na pagina inicial

--- Script/main.js
+++ Script/main.js
@@ -17,3 +17,31 @@ window.addEventListener("scroll", () => {
 
   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 });
+
+function sobre(select) {
+  const inicio_bt = document.getElementById("beggin");
+  const projetos_bt = document.getElementById("projetos");
+  const premios_bt = document.getElementById("premios");
+  const agente1 = document.getElementById("agente1");
+  const agente2 = document.getElementById("agente2");
+  const agente3 = document.getElementById("agente3");
+  switch (select) {
+    case "inicio":
+      inicio_bt.classList.add("selected");
+      premios_bt.classList.remove("selected");
+      projetos_bt.classList.remove("selected");
+      agente1.innerText = "esse texto e muito legal ne";
+      console.log("foi");
+      break;
+    case "projetos":
+      projetos_bt.classList.add("selected");
+      inicio_bt.classList.remove("selected");
+      premios_bt.classList.remove("selected");
+      break;
+    case "premios":
+      premios_bt.classList.add("selected");
+      inicio_bt.classList.remove("selected");
+      projetos_bt.classList.remove("selected");
+      break;
+  }
+}
