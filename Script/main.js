845c2ecda91c77388e99434202cda758381d4e60
Braian-jm <braianjmoura@gmail.com>
2025-10-19 18:08:55 -0300

    estilo nos botoes de sobre

--- Script/main.js
+++ Script/main.js
@@ -1,22 +1,21 @@
-let lastScrollTop = 0;
-const navbar = document.getElementById("navbar");
-const logomini = document.getElementById("logo-mini");
-window.addEventListener("scroll", () => {
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+// lista das seções que serão alternadas
+const secoes = ["inicio", "projetos", "premios"];
+let indiceAtual = 0;
+let intervalo;
 
-  // rolando pra baixo → encolhe
-  if (scrollTop > lastScrollTop) {
-    navbar.classList.add("shrink");
-    logomini.classList.add("shrink");
-  }
-  // rolando pra cima → volta ao tamanho original
-  else {
-    navbar.classList.remove("shrink");
-    logomini.classList.remove("shrink");
-  }
+function trocarAutomaticamente() {
+  indiceAtual = (indiceAtual + 1) % secoes.length;
+  sobre(secoes[indiceAtual]);
+}
 
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-});
+function iniciarTrocaAutomatica() {
+  intervalo = setInterval(trocarAutomaticamente, 10000);
+}
+
+function resetarTimer() {
+  clearInterval(intervalo);
+  iniciarTrocaAutomatica();
+}
 
 function sobre(select) {
   const inicio_bt = document.getElementById("beggin");
@@ -25,23 +24,41 @@ function sobre(select) {
   const agente1 = document.getElementById("agente1");
   const agente2 = document.getElementById("agente2");
   const agente3 = document.getElementById("agente3");
+
+  // 🔁 reinicia o timer toda vez que o usuário clicar
+  resetarTimer();
+
   switch (select) {
     case "inicio":
       inicio_bt.classList.add("selected");
       premios_bt.classList.remove("selected");
       projetos_bt.classList.remove("selected");
       agente1.innerText = "esse texto e muito legal ne";
-      console.log("foi");
+      agente2.innerText = "esse aq tmb e legal";
+      agente3.innerText = "e esse em";
+      indiceAtual = 0;
       break;
     case "projetos":
       projetos_bt.classList.add("selected");
       inicio_bt.classList.remove("selected");
       premios_bt.classList.remove("selected");
+      agente1.innerText = "proj 1";
+      agente2.innerText = "proj 2";
+      agente3.innerText = "proj 3";
+      indiceAtual = 1;
       break;
     case "premios":
       premios_bt.classList.add("selected");
       inicio_bt.classList.remove("selected");
       projetos_bt.classList.remove("selected");
+      agente1.innerText = "pre 1";
+      agente2.innerText = "pre 2";
+      agente3.innerText = "pre 3";
+      indiceAtual = 2;
       break;
   }
 }
+
+window.addEventListener("load", () => {
+  iniciarTrocaAutomatica();
+});
